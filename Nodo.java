e360ba687daf025530b0b127aeaa29fd9dc3aa5c
imanol-77 <imanol.mejia@est.iudigital.edu.co>
2026-05-17 18:34:09 -0500

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,10 +1,16 @@
 public class Nodo {
 
+    // Objeto pizza almacenado en el nodo
     Pizza pizza;
+
+    // Puntero al siguiente nodo de la lista ligada
     Nodo siguiente;
 
     public Nodo(Pizza pizza) {
+
         this.pizza = pizza;
+
+        // El nodo inicia sin apuntar a otro nodo
         this.siguiente = null;
     }
 }
